10899cabd58cb9d0c50bc09a74293a82d27a0876
Leon-Schoenbrunn1 <leon.schoenbrunn@rwth-aachen.de>
2024-04-28 22:49:52 +0200

    Revert "Merge pull request #2 from OMFSdigital/usage_example"
    This reverts commit c836a684bfac05b3847607a947af602255907a1d, reversing
    changes made to 8f085e42868dfee2eff9022a4e42c1628a6f1dec.

--- src/pan_ai_x/pan_ai_x.py
+++ src/pan_ai_x/pan_ai_x.py
@@ -11,30 +11,28 @@ from PIL import Image
 from torch.utils.tensorboard import SummaryWriter
 
 class PAN_AI_X:
-    def __init__(self, weights_path=None):
+    def __init__(self, weights_path):
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-        self.model = self._load_model(weights_path)
+        self.model = self._load_model(weights_path).to(self.device)
+        self.model.eval()
         self.transformval = transforms.Compose([
-            transforms.Resize((224,224)),
+            transforms.Resize(256),
+            transforms.CenterCrop(224),
             transforms.ToTensor(),
             transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
         ])
         self.transformtrain = transforms.Compose([
-            transforms.RandomRotation(15),
+            transforms.RandomResizedCrop(224),
             transforms.RandomHorizontalFlip(),
-            transforms.Resize((224,224)),
             transforms.ToTensor(),
             transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
         ])
 
     def _load_model(self, weights_path):
-        if weights_path:
-            model = models.resnet50(weights=None)
-            num_ftrs = model.fc.in_features
-            model.fc = nn.Linear(num_ftrs, 1)
-            model.load_state_dict(torch.load(weights_path))
-        else:
-            model = None
+        model = models.resnet50(weights=None)
+        num_ftrs = model.fc.in_features
+        model.fc = nn.Linear(num_ftrs, 1)
+        model.load_state_dict(torch.load(weights_path))
         return model
         
     def _load_model_train(self):
@@ -44,24 +42,17 @@ class PAN_AI_X:
         return model
 
     def predict(self, image_path):
-        if self.model:
-            self.model.to(self.device)
-            self.model.eval()
-            image = Image.open(image_path).convert("RGB")
-            image_tensor = self.transformval(image).unsqueeze(0).to(self.device)
-            
-            with torch.no_grad():
-                output = self.model(image_tensor)
-                prediction = torch.sigmoid(output).item()
-            
-            return prediction
-        else:
-            print('No model has been loaded via a weight_path.')
+        image = Image.open(image_path).convert("RGB")
+        image_tensor = self.transformval(image).unsqueeze(0).to(self.device)
+        
+        with torch.no_grad():
+            output = self.model(image_tensor)
+            prediction = torch.sigmoid(output).item()
+        
+        return prediction
 
     def train(self, train_dataset, val_dataset, test_dataset):
         self.model = self._load_model_train().to(self.device)
-        
-        timestamp = datetime.now().strftime('%Y%m%d_%H%M%S')
         
         train_set = datasets.ImageFolder(root=train_dataset, transform=self.transformtrain)
         train_loader = DataLoader(train_set, batch_size=32, shuffle=True)
@@ -129,7 +120,7 @@ class PAN_AI_X:
 
             if val_auc > best_auc:
                 best_auc = val_auc
-                torch.save(self.model.state_dict(), 'ToothPredictor_BestAUC_' + str(timestamp) + '.pth')
+                torch.save(self.model.state_dict(), 'ToothPredictor_BestAUC.pth')
         
         # Testing
         print('TESTING:')
@@ -165,7 +156,7 @@ class PAN_AI_X:
         print(f"Test AUC: {test_auc}")
         
         # Save the final trained model
-        torch.save(self.model.state_dict(), 'ToothPredictor_final_' + str(timestamp) + '.pth')
+        torch.save(self.model.state_dict(), 'ToothPredictor_final.pth')
 
 
 # Example usage:
